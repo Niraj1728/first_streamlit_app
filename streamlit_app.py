906a0dc26f8b503cf64996643b6a265c7a9bd9dd
Niraj1728 <127056005+Niraj1728@users.noreply.github.com>
2023-03-05 15:47:37 -0600

    Update streamlit_app.py
    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 streamlit.title("nene thope")
 streamlit.header(':japanese_ogre: nen')
 streamlit.text('nvve')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
