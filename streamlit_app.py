b5f0a6944d3c930ab3314c38f91f8864475ac0b2
Niraj1728 <127056005+Niraj1728@users.noreply.github.com>
2023-06-05 21:31:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,6 +79,4 @@ if streamlit.button('Add a fruit to the list'):
                    
                    
                    
-  
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding Jackfruit')
+ 
