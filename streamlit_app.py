a8bf7951102140dcd5215eee907a429a1e8f965a
Niraj1728 <127056005+Niraj1728@users.noreply.github.com>
2023-06-05 21:40:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 
 streamlit.title("My parents new healthy diner")
 
-streamlit.header('Breakfast Favorites')
+streamlit.header('Breakfast Favorites') 
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
